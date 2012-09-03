67b939232c9ed15c3c5007450a6aa26a99d0d6be
unknown <Thomas@Thomas-PC.(none)>
2012-09-03 18:06:18 +0200

    Initiale Dateierstellung

---
+++ Localization.lua
@@ -0,0 +1,15 @@
+-- English localization (Default)
+DSG_LOOT = "DSG Loot"
+RAIDLOOT_ROLL_PATTERN = "(.+) rolls (%d+) %((%d+)%-(%d+)%)"
+
+
+-- German localization
+if GetLocale() == "deDE" then
+	RAIDLOOT_ROLL_PATTERN = "(.+) w\195\188rfelt. Ergebnis: (%d+) %((%d+)%-(%d+)%)"
+-- French localization
+elseif GetLocale() == "frFR" then
+	RAIDLOOT_ROLL_PATTERN = "(%S+) obtient un (%d+) %((%d+)%-(%d+)%)"
+-- Spanish localization
+elseif GetLocale() == "esES" or GetLocale() == "esMX" then
+	RAIDLOOT_ROLL_PATTERN = "(%S+) tira los dados y obtiene (%d+) %((%d+)%-(%d+)%)"
+end
