d7f619b4a57eeec8f8690230cf2192fa8db050f7
Accidant <Thomas@Thomas-PC.(none)>
2012-09-04 23:13:27 +0200

    Arbeiten an der Gui weiter fortgefuehrt

--- DSGLoot-Master.lua
+++ DSGLoot-Master.lua
@@ -1,5 +1,6 @@
-local playerList;
-local lootList;
+local playerList = {};
+local lootList = {};
+local raidId = nil;
 
 LootDB = {}
 DSGLootMasterConfig = {
@@ -12,17 +13,17 @@ local DefaultDSGLootMasterConfig = {
 }
 
 local exampleDB = {
-	"ID" = {
-		0 = {
+	["ID"] = {
+		[0] = {
 			Item = "ITEMLINK",
 			rolls = {
-				"PLAYER1" = {
+				["PLAYER1"] = {
 					roll = {
 						level = "NEED",
 						result = 89,
 					}
 				},
-				"PLAYER2" = {
+				["PLAYER2"] = {
 					roll = {
 						level = "GREED",
 						result = 25,
@@ -48,7 +49,14 @@ end
 
 
 function DSGLootMaster_OnLoad(self)
+	self.itemFrames = {};
+	self.expandedRolls = {};
+	self.usedPlayerFrames = {};
+	self.unusedPlayerFrames = {};
+	self.highlightedRolls = {};
+
 	assignDefaultOptions();
+	getRaidId();
 	self:RegisterEvent("CHAT_MSG_ADDON");
 	self:RegisterEvent("LOOT_OPENED");
 	self:RegisterEvent("LOOT_CLOSED");
@@ -59,9 +67,11 @@ function DSGLootMaster_OnLoad(self)
 end
 
 
-function DSGLootMaster_OnEvent(self, event)
+function DSGLootMaster_OnEvent(self, event, ...)
+	args = ...;
+
 	if event == "CHAT_MSG_ADDON" then
-		prefix, message, distribution, sender = ...;
+		prefix, message, distribution, sender = args;
 		if(prefix == DSGLOOT_PREFIX) then
 			revievedAddonMsg(message, sender);
 		end
@@ -75,30 +85,187 @@ function DSGLootMaster_OnEvent(self, event)
 	end	
 end
 
+--Full Update of the DSGLootMasterFrame
 function DSGLootMaster_FullUpdate(self)
-	local numItems = 2;
+	DSGLootMaster_RecyclePlayerFrames(self);
+	local numItems = GetNumLootItems();
 	local previous = nil;
 	for i=1, numItems do
-		local frame = CreateFrame("BUTTON", nil, self.ScrollFrame.ScrollChild, "DSGLootItemTemplate");
-	
-		DSGLootMaster_UpdateItemFrame(self, frame, GetContainerItemLink(3,12+i));
-		if (previous) then
-			frame:SetPoint("TOPLEFT", previous, "BOTTOMLEFT", 0, -5);
-		else
-			frame:SetPoint("TOPLEFT", 0, -2);
+		if (LootSlotHasItem(i)) then	
+			local frame = self.itemFrames[i];
+			if (not frame) then
+				frame = CreateFrame("BUTTON", nil, self.ScrollFrame.ScrollChild, "DSGLootItemTemplate");
+				self.itemFrames[i] = frame;
+			end			
+			
+			frame.rollId = i;
+			frame.itemIdx = i;
+			frame.itemLink = GetLootSlotLink(i);	
+
+			--Dirty fix cause LootSlotHasItems doesn't detect coins
+			--If Fixed: can be removed
+			if frame.itemLink ~= nil then
+				DSGLootMaster_UpdateItemFrame(self, frame);
+				frame:Show();
+				
+				if (previous) then
+					frame:SetPoint("TOPLEFT", previous, "BOTTOMLEFT", 0, -5);
+				else
+					frame:SetPoint("TOPLEFT", 0, -2);
+				end
+				
+				if (self.expandedRolls[frame.rollId]) then
+					local firstFrame, lastFrame = DSGLootMaster_UpdatePlayerFrames(self, frame.itemIdx);
+					if ( firstFrame ) then
+						firstFrame:SetPoint("TOPLEFT", frame, "BOTTOMLEFT", 0, -2);
+						previous = lastFrame;
+					else
+						previous = frame;
+					end
+				else
+					previous = frame;
+				end
+			end
 		end
-		frame:Show();
-		previous = frame;
+	end
+	for i=numItems + 1, #self.itemFrames do
+		self.itemFrames[i]:Hide();
 	end
 end
 
-function DSGLootMaster_UpdateItemFrame(self, itemFrame, item)
-	local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture = GetItemInfo(item);
+-- Recycles all PlayerFrames
+function DSGLootMaster_RecyclePlayerFrames(self)
+	for i=1, #self.usedPlayerFrames do
+		local frame = self.usedPlayerFrames[i];
+		frame.itemIdx = nil;
+		frame.playerIdx = nil;
+		frame:Hide();
+		table.insert(self.unusedPlayerFrames, frame);
+	end
+	table.wipe(self.usedPlayerFrames);
+end
+
+
+function DSGLootMaster_UpdateItemFrame(self, itemFrame)
+	local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture = GetItemInfo(itemFrame.itemLink);
+	local expanded = self.expandedRolls[itemFrame.rollId];
+	
+	if (expanded) then
+		itemFrame.ToggleButton:SetNormalTexture("Interface\\Buttons\\UI-MinusButton-UP");
+		itemFrame.ToggleButton:SetPushedTexture("Interface\\Buttons\\UI-MinusButton-Down");
+		itemFrame.ToggleButton:SetDisabledTexture("Interface\\Buttons\\UI-MinusButton-Disabled");
+	else
+		itemFrame.ToggleButton:SetNormalTexture("Interface\\Buttons\\UI-PlusButton-UP");
+		itemFrame.ToggleButton:SetPushedTexture("Interface\\Buttons\\UI-PlusButton-Down");
+		itemFrame.ToggleButton:SetDisabledTexture("Interface\\Buttons\\UI-PlusButton-Disabled");
+	end
+	
 	itemFrame.Icon:SetTexture(itemTexture);
 	local colorInfo = ITEM_QUALITY_COLORS[itemRarity];
 	itemFrame.IconBorder:SetVertexColor(colorInfo.r, colorInfo.g, colorInfo.b);
 	itemFrame.ItemName:SetText(itemName);
 	itemFrame.ItemName:SetVertexColor(colorInfo.r, colorInfo.g, colorInfo.b);
+	
+	if ( self.highlightedRolls[rollID] ) then
+		itemFrame.ActiveHighlight:Show();
+	else
+		itemFrame.ActiveHighlight:Hide();
+	end
+end
+
+function DSGLootMaster_GetPlayerFrame(self)
+	local frame = table.remove(self.unusedPlayerFrames);
+	if (not frame) then
+		frame = CreateFrame("BUTTON", nil, self.ScrollFrame.ScrollChild, "DSGLootPlayerTemplate");
+	end
+	table.insert(self.usedPlayerFrames, frame);
+	return frame;
+end
+
+function DSGLootMaster_UpdatePlayerFrames(self, itemIdx)
+	local firstFame, lastFrame;
+	
+	for i=1, GetNumGroupMembers() do
+		local candidate, _ = GetMasterLootCandidate(i, itemIdx);
+		playerList[i] = candidate;
+	
+		local frame = DSGLootMaster_GetPlayerFrame(self);
+		if (lastFrame) then
+			frame:SetPoint("TOPLEFT", lastFrame, "BOTTOMLEFT", 0, -1);
+		else
+			frame:ClearAllPoints();
+		end
+		
+		firstFrame = firstFrame or frame;
+		lastFrame = frame;
+		
+		frame.itemIdx = itemIdx;
+		frame.playerIdx = i;
+		DSGLootMaster_UpdatePlayerFrame(self, frame);
+		frame:Show();
+	end
+	return firstFrame, lastFrame;
+end
+
+function DSGLootMaster_UpdatePlayerFrame(self, playerFrame)
+	local name, rank, subgroup, level, class, filename, zone = GetRaidRosterInfo(playerFrame.playerIdx);
+	if ( playerFrame.playerIdx ) then
+		if (playerFrame.playerIdx % 2 == 1) then
+			playerFrame.AlternatingBG:Show();
+		else
+			playerFrame.AlternatingBG:Hide();
+		end
+		
+		if ( name ) then
+			playerFrame.PlayerName:SetText(name);
+			local classColor = RAID_CLASS_COLORS[filename];
+			playerFrame.PlayerName:SetVertexColor(classColor.r, classColor.g, classColor.b);
+		else
+			playerFrame.PlayerName:SetText(UNKNOWNOBJECT);
+			playerFrame.PlayerName:SetVertexColor(RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b);
+		end
+	else
+		playerFrame.AlternatingBG:Show();
+		playerFrame.PlayerName:SetText(LOOT_HISTORY_ALL_PASSED);
+		playerFrame.PlayerName:SetVertexColor(RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b);
+		playerFrame.RollIcon.Texture:SetTexture("Interface\\Buttons\\UI-GroupLoot-Pass-Up");
+		playerFrame.RollIcon.tooltip = PASS;
+		playerFrame.RollText:SetText("");
+		playerFrame.RollIcon:SetPoint("RIGHT", playerFrame, "RIGHT", -2, -1);
+		playerFrame.WinMark:Hide();
+	end
+end
+
+function DSGLootMaster_ToggleRollExpanded(self, rollID)
+	DSGLootMaster_SetRollExpanded(self, rollID, not self.expandedRolls[rollID]);
+end
+
+function DSGLootMaster_SetRollExpanded(self, rollID, isExpanded)
+	self.expandedRolls[rollID] = isExpanded;
+	DSGLootMaster_FullUpdate(self);
+end
+
+-- Gets the current Raid id
+function getRaidId()
+	local name = GetInstanceInfo();
+	for i = 1, GetNumSavedInstances() do
+		local savedName, id = GetSavedInstanceInfo(i);
+		if savedName == name then
+			raidId = id
+		end
+	end
+end
+
+-- Gets all Raidmember
+function getRaidMembers()
+	numGrpMembers = 0;
+	for i =1, GetNumGroupMembers() do
+		local candidate, _ = GetMasterLootCandidate(i)
+		if not (candidate == nil) then
+			numGrpMembers = numGrpMembers + 1;
+			raidMembers[numGrpMembers] = candidate;
+		end
+	end
 end
 
 function recievedAddonMsg(message, sender)
@@ -122,6 +289,6 @@ function rollForLevel(level)
 end
 
 function registerRollAndReply(item, result, level)
-	SendAddonMessage(DSGLOOT_PREFIX, string.format(DSGLOOT_ITEMROLL_REPLY, item, level, result);
+	SendAddonMessage(DSGLOOT_PREFIX, string.format(DSGLOOT_ITEMROLL_REPLY, item, level, result));
 	--REGISTER ROLL;
 end
